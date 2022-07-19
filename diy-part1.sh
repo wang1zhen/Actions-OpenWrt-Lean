96b390161d46164f22dee72d3c3cf5d361d6a5bd
wang1zhen <wang1zhendire@hotmail.com>
2022-07-19 22:36:28 +0800

    update .config and add zzz-default-settings

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
